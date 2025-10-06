82677fad31c5bb5364683c7cf057a8743d843f5f
Juan Esteban Dulcey Gómez <juanesdulcey05@gmail.com>
2025-10-06 11:37:16 -0500

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     app_version: str = "1.0.0"
     
     host: str = "0.0.0.0"
-    port: int = 443
+    port: int = 8000
     debug: bool = False
     
     allow_origins: list[str] = ["*"]
