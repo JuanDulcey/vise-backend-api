1fc23ee02919a4e3e48ec3231c56b44d610be366
Juan Esteban Dulcey Gómez <juanesdulcey05@gmail.com>
2025-10-27 15:01:37 +0000

    nuevo ambiente virtual completo

---
+++ telemetry.py
@@ -0,0 +1,7 @@
+# telemetry.py
+import os
+from azure.monitor.opentelemetry import configure_azure_monitor
+
+configure_azure_monitor(
+    connection_string=os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING")
+)
