a333460bc4237d96ef17e0abe6355a8a2ddb775c
Juan Esteban Dulcey Gómez <juanesdulcey05@gmail.com>
2025-10-27 14:59:16 +0000

    se modifico el main para añadir la integracion de azure monitor

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ from app.routers import clientes
 
 # OpenTelemetry setup
 import otel_setup
+from azure.monitor.opentelemetry import configure_azure_monitor
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 
 
+# Configura Azure Monitor directamente con el string de conexión
+configure_azure_monitor(
+    connection_string="InstrumentationKey=235ce92f-07ea-41f2-8015-bed44710efa7;IngestionEndpoint=https://mexicocentral-0.in.applicationinsights.azure.com/;LiveEndpoint=https://mexicocentral.livediagnostics.monitor.azure.com/;ApplicationId=d47e9fa7-a0c0-473c-a61f-b2fa8ca7bd89"
+)
 
 app = FastAPI(
     title="API Clientes",
@@ -17,10 +22,11 @@ app = FastAPI(
     redoc_url="/redoc"
 )
 
+# Instrumentación de FastAPI y solicitudes HTTP
 FastAPIInstrumentor.instrument_app(app)
 RequestsInstrumentor().instrument()
 
-
+# Middleware CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -29,7 +35,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+# Rutas
 app.include_router(clientes.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello from FastAPI with App Insights!"}
+
 
 if __name__ == "__main__":
     import uvicorn
